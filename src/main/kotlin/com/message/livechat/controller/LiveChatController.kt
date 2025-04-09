8d6695cf5e78349e9aba51aedbfd215eabcb7b24
AnselmoAvesJunior <ansjunior@msn.com>
2025-04-08 21:49:31 -0300

    feat: refactored to kafka

--- src/main/kotlin/com/message/livechat/controller/LiveChatController.kt
+++ src/main/kotlin/com/message/livechat/controller/LiveChatController.kt
@@ -4,15 +4,12 @@ import com.message.livechat.domain.ChatInput
 import com.message.livechat.domain.ChatOutput
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.stereotype.Controller
-import org.springframework.web.util.HtmlUtils
 
 @Controller
 class LiveChatController(
     private val kafkaTemplate: KafkaTemplate<String, String>
 ) {
-
     @MessageMapping("/new-message")
     fun newMessage(input: ChatInput) {
         kafkaTemplate.send("livechat-topic", input.user + ": " + input.message)
